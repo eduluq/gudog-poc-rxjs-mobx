c9658a532a61805dc136d6ee9d82b1fcb984a455
Eduardo Luque <edu@gudog.com>
2020-09-24 19:07:19 +0200

    remove themeObservables from setup

--- src/models/setup-root-store.ts
+++ src/models/setup-root-store.ts
@@ -3,13 +3,12 @@ import { RootStoreModel, RootStore } from "./root-store";
 import * as gudogRXJS from "../libraries/rxjs-gudog";
 import startupObservables from "../rxjs/startupObservables";
 import navigationObservables from "../rxjs/navigationObservables";
-import themeObservables from "../rxjs/themeObservables";
 
 export async function setupRootStore() {
   let rootStore: RootStore = RootStoreModel.create({
     startupCompleted: false,
     navigationMode: "owner",
-    theme: "light"
+    theme: "light",
   });
 
   const actionsStream = new Subject();
@@ -17,7 +16,7 @@ export async function setupRootStore() {
   gudogRXJS.configure({
     store: rootStore,
     stream: actionsStream,
-    observables: [startupObservables, navigationObservables, themeObservables]
+    observables: [startupObservables, navigationObservables],
   });
 
   return rootStore;
